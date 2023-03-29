5926b187a8185497676be54abda601f3c339a9a4
Cyryl Majewski <maevsky.cyril@gmail.com>
2023-03-29 22:08:42 +0200

    finished task

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
-// Expected result:
-//
+const initialCapacities = {
+  EUR: 50000,
+  USD: 43000,
+};
+
+const totals = [
+  {
+    total: {
+      amount: {
+        asset: { currency: "GBP", type: "Cash" },
+        quantity: 100000,
+      },
+    },
+  },
+  {
+    total: {
+      amount: {
+        asset: { currency: "EUR", type: "Cash" },
+        quantity: -200000,
+      },
+    },
+  },
+];
+
+const finalResult = totals.reduce((current, item) => {
+    if (current.hasOwnProperty(item.total.amount.asset.currency)) {
+        current[item.total.amount.asset.currency] += item.total.amount.quantity;
+    }
+
+    return current;
+}, initialCapacities);
+
+console.log(finalResult);
+
 // {
 //   EUR: -150000,
 //   USD: 43000
 // }
-
-const initialCapacities = {
-  EUR: 50000,
-  USD: 43000
-};
-
-const arr = [{
-  total: {
-    amount: {
-      asset: {currency: 'GBP', type: 'Cash'},
-      quantity: 100000,
-    },
-  },
-}, {
-  total: {
-    amount: {
-      asset: { currency: 'EUR', type: 'Cash' },
-      quantity: -200000,
-    },
-  },
-}]
